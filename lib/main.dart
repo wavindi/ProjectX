015deefee4ac59f893c6dba05d2b11bef50f0cab
wavindi <143713661+wavindi@users.noreply.github.com>
2025-01-25 11:55:08 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart'; // Ensure this file is generated using FlutterFire CLI
 import 'screens/home/home_page.dart';
 import 'screens/splash/splash_screen.dart';
 import 'screens/auth/welcome_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,11 +27,24 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
+      // Checking if the user is logged in
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          } else if (snapshot.hasData) {
+            // If the user is logged in, navigate to HomePage
+            return const HomePage();
+          } else {
+            // If the user is not logged in, navigate to WelcomeScreen (Login/Signup)
+            return const WelcomeScreen();
+          }
+        },
+      ),
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
         '/home': (context) => const HomePage(),
+        '/welcome': (context) => const WelcomeScreen(),
       },
     );
   }
